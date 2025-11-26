4c1be5eef6dd3a6dc6be68b9bf2e8f983e379170
Yago Martins <ylm@icomp.ufam.edu.br>
2025-11-25 21:08:00 -0400

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/desafiotecnico/matera/MateraApplication.java
@@ -0,0 +1,13 @@
+package com.desafiotecnico.matera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiotecnico/matera/MateraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiotecnico.matera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

edcf4985292b82d49c966439c6bd85aa36564df1
Yago Martins <ylm@icomp.ufam.edu.br>
2025-11-26 18:48:11 -0400

    feat: add global exception handler

---
+++ src/main/java/com/desafiotecnico/matera/shared/exception/InsufficientBalanceException.java
@@ -0,0 +1,8 @@
+package com.desafiotecnico.matera.shared.exception;
+
+public class InsufficientBalanceException extends BusinessException {
+
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
